839da781faaef82733a1653c171f09c543d54678
Ayush Jain <ayushjain2911@gmail.com>
2016-06-18 11:45:21 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, request
 from twilio.util import TwilioCapability
+from twilio.rest import TwilioRestClient
+
 import twilio.twiml
 
 # Account Sid and Auth Token can be found in your account dashboard
