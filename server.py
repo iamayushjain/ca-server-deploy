88281eae8d033e6b0bb6166553e46ee3b8018be2
Ayush Jain <ayushjain2911@gmail.com>
2016-06-18 12:42:58 +0530

    Update server.py

--- server.py
+++ server.py
@@ -61,8 +61,7 @@ def call():
   elif to.startswith("conference:"):
     resp.dial(callerId=from_value).conference('MyConference')
     participants = client_rest.participants('CFbbe46ff1274e283f7e3ac1df0072ab39').list()
-    resp = twilio.twiml.Response()
-    resp.say("Welcome to Culture Alley Hello English")
+    print len(participants)
  else:
     # client -> PSTN
      resp.dial(to, callerId=caller_id)
