a6b1e98b43d94b522e5012561bfe7758353d718b
Ayush Jain <ayushjain2911@gmail.com>
2016-06-21 11:04:52 +0530

    Update server.py

--- server.py
+++ server.py
@@ -60,10 +60,9 @@ def call():
   elif to.startswith("conference:"):
     resp.dial(callerId=from_value).conference('MyConference')
     #participants = client_rest.participants('CFbbe46ff1274e283f7e3ac1df0072ab39').list()
-    conferences = client_rest.conferences.list()
-    for conference in conferences:
-        print conference.sid
-
+    # conferences = client_rest.conferences.list()
+    # for conference in conferences:
+    #     print conference.sid
   else:
     # client -> PSTN
      resp.dial(to, callerId=caller_id)
