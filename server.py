38448abf5be475b81abbba227b8e8b690866129c
Ayush Jain <ayushjain2911@gmail.com>
2016-06-21 11:51:21 +0530

    Update server.py

--- server.py
+++ server.py
@@ -62,7 +62,7 @@ def call():
     #participants = client_rest.participants('CFbbe46ff1274e283f7e3ac1df0072ab39').list()
     conferences = client_rest.conferences.list()
     for conference in conferences:
-      print conference.sid
+      return str(conference.sid)
         # welcome()
   else:
     # client -> PSTN
