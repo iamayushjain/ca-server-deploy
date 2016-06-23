2b1ff0e8e4590fb83b317a8839572a9ad97d540f
Ayush Jain <ayushjain2911@gmail.com>
2016-06-23 12:22:16 +0530

    Update server.py

--- server.py
+++ server.py
@@ -59,7 +59,7 @@ def call():
     resp.dial(callerId=from_value).client(to[7:])
   elif to.startswith("conference:"):
     resp.dial(callerId=from_value).conference('MyConference')
-    resp.dial(callerId=from_value).conference('MyConference').
+    #resp.dial(callerId=from_value).conference('MyConference').
     #participants = client_rest.participants('CFbbe46ff1274e283f7e3ac1df0072ab39').list()
     # conferences = client_rest.conferences.list()
     # for conference in conferences:
